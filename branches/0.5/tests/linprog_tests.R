44921b5dcd47d008292ed86451503e94e64b5981
arne.henningsen <unknown@noreply.com>
2010-04-23 02:35:33 +0000

    added tests for calls to solveLP() with argument solve.dual set to TRUE
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/linprog@116 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/0.5/tests/linprog_tests.R
+++ branches/0.5/tests/linprog_tests.R
@@ -15,10 +15,17 @@ print( result1a )
 summary( result1a )
 # print all elements of the returned object
 print.default( result1a )
+# also estimate the dual problem
+result1aD <- solveLP( cvec, bvec, Amat, TRUE, verbose = 1, solve.dual = TRUE )
+result1aD$con
+all.equal( result1a[-12], result1aD[-c(2,13)] )
 
 # estimation with verbose = TRUE
 result1b <- solveLP( cvec, bvec, Amat, TRUE, verbose = 4 )
 all.equal( result1a, result1b )
+# also estimate the dual problem
+result1bD <- solveLP( cvec, bvec, Amat, TRUE, verbose = 4, solve.dual = TRUE )
+all.equal( result1aD, result1bD )
 
 # estimation with lpSolve
 result1c <- solveLP( cvec, bvec, Amat, TRUE, lpSolve = TRUE, verbose = 4 )
@@ -27,6 +34,10 @@ print( result1c )
 summary( result1c )
 # print all elements of the returned object
 print.default( result1c )
+# also estimate the dual problem
+result1cD <- solveLP( cvec, bvec, Amat, TRUE, lpSolve = TRUE, solve.dual = TRUE )
+result1cD$con
+all.equal( result1c[-8], result1cD[-c(6,9)] )
 
 # using argument const.dir
 const.dir <- c( ">=", ">=", ">=" )
@@ -34,12 +45,22 @@ result1d <- solveLP( cvec, -bvec, -Amat, maximum = TRUE, verbose = 1,
    const.dir = const.dir )
 print( result1d )
 all.equal( result1a[-8], result1d[-8] )
+# also estimate the dual problem
+result1dD <- solveLP( cvec, -bvec, -Amat, TRUE, verbose = 1,
+   const.dir = const.dir, solve.dual = TRUE )
+result1dD$con
+all.equal( result1aD[-9], result1dD[-9] )
 
 # using argument const.dir and lpSolve
 result1e <-solveLP( cvec, -bvec, -Amat, maximum = TRUE, verbose = 1,
    const.dir = const.dir, lpSolve = TRUE )
 print( result1e )
 all.equal( result1c[-5], result1e[-5] )
+# also estimate the dual problem
+result1eD <- solveLP( cvec, -bvec, -Amat, TRUE, verbose = 1,
+   const.dir = const.dir, lpSolve = TRUE, solve.dual = TRUE )
+result1eD$con
+all.equal( result1cD[-5], result1eD[-5] )
 
 
 ## Example 2
@@ -57,10 +78,17 @@ print( result2a )
 summary( result2a )
 # print all elements of the returned object
 print.default( result2a )
+# also estimate the dual problem
+result2aD <- solveLP( cvec, bvec, Amat, verbose = 1, solve.dual = TRUE )
+result2aD$con
+all.equal( result2a[-12], result2aD[-c(2,13)] )
 
 # estimation with verbose = TRUE
 result2b <- solveLP( cvec, bvec, Amat, verbose = 4 )
 all.equal( result1a, result1b )
+# also estimate the dual problem
+result2bD <- solveLP( cvec, bvec, Amat, verbose = 4, solve.dual = TRUE )
+all.equal( result2aD, result2bD )
 
 # estimation with lpSolve
 result2c <- solveLP( cvec, bvec, Amat, lpSolve = TRUE, verbose = 4 )
@@ -69,6 +97,11 @@ print( result2c )
 summary( result2c )
 # print all elements of the returned object
 print.default( result2c )
+# also estimate the dual problem
+result2cD <- solveLP( cvec, bvec, Amat, lpSolve = TRUE, verbose = 4,
+   solve.dual = TRUE )
+result2cD$con
+all.equal( result2c[-8], result2cD[-c(6,9)] )
 
 # using argument const.dir
 const.dir <- c( ">=", ">=", "<=" )
@@ -76,11 +109,21 @@ result2d <- solveLP( cvec, abs( bvec ), abs( Amat ), verbose = 1,
    const.dir = const.dir )
 print( result2d )
 all.equal( result2a[-8], result2d[-8] )
+# also estimate the dual problem
+result2dD <- solveLP( cvec, abs( bvec ), abs( Amat ), verbose = 1,
+   const.dir = const.dir, solve.dual = TRUE )
+result2dD$con
+all.equal( result2aD[-9], result2dD[-9] )
 
 # using argument const.dir and lpSolve
 result2e <- solveLP( cvec, abs( bvec ), abs( Amat ), verbose = 1,
    const.dir = const.dir, lpSolve = TRUE )
 print( result2e )
 all.equal( result2c[-5], result2e[-5] )
+# also estimate the dual problem
+result2eD <- solveLP( cvec, abs( bvec ), abs( Amat ), verbose = 1,
+   const.dir = const.dir, lpSolve = TRUE, solve.dual = TRUE )
+result2eD$con
+all.equal( result2cD[-5], result2eD[-5] )
 
 
